95ba27746304b0fb0b00c2256d6d486562dfa142
aadritraj <176519217+aadritraj@users.noreply.github.com>
2026-08-25 17:50:51 +0200

    support terminal launching

--- src/launch.rs
+++ src/launch.rs
@@ -1,7 +1,6 @@
-// TODO: handle desktop entries with Terminal=true
 use std::{
-    io,
-    path::PathBuf,
+    env, io,
+    path::{Path, PathBuf},
     process::{Command, Stdio},
 };
 
@@ -24,6 +23,7 @@ pub struct DesktopEntryLaunch {
     pub appid: String,
     pub args: Vec<String>,
     pub working_directory: Option<PathBuf>,
+    pub terminal: bool,
 }
 
 #[derive(Debug, Error)]
@@ -32,6 +32,8 @@ pub enum LaunchError {
     InvalidExec(#[from] freedesktop_desktop_entry::ExecError),
     #[error("launch command is empty")]
     EmptyCommand,
+    #[error("no terminal emulator is available")]
+    NoTerminal,
     #[error("could not spawn application: {0}")]
     Spawn(#[from] io::Error),
 }
@@ -46,6 +48,7 @@ impl DesktopEntryLaunch {
             appid,
             args,
             working_directory,
+            terminal: entry.terminal(),
         })
     }
 }
@@ -65,17 +68,82 @@ impl ActionExecutor for SystemExecutor {
                     .args
                     .split_first()
                     .ok_or(LaunchError::EmptyCommand)?;
-                let mut command = Command::new(program);
-                command.args(args);
+                let mut command = if desktop_entry.terminal {
+                    let terminal = resolve_terminal().ok_or(LaunchError::NoTerminal)?;
+                    let mut command = Command::new(terminal.executable);
+                    command.args(terminal.prefix);
+                    command.arg(program).args(args);
+                    command
+                } else {
+                    let mut command = Command::new(program);
+                    command.args(args);
+                    command
+                };
                 command
                     .stdin(Stdio::null())
                     .stdout(Stdio::null())
                     .stderr(Stdio::null());
+                // gnome-terminal does not respect this! they opt to have a --working-directory flag instead
+                // i will not bother.
                 if let Some(path) = &desktop_entry.working_directory {
                     command.current_dir(path);
                 }
                 command.spawn().map(|_| ()).map_err(LaunchError::from)
             }
         }
+    }
+}
+
+#[derive(Debug)]
+struct TerminalCommand {
+    executable: PathBuf,
+    prefix: Vec<String>,
+}
+
+fn resolve_terminal() -> Option<TerminalCommand> {
+    let mut candidates = env::var_os("TERMINAL")
+        .map(PathBuf::from)
+        .into_iter()
+        .chain(std::iter::once(PathBuf::from("x-terminal-emulator")))
+        .chain(
+            [
+                "ghostty",
+                "kitty",
+                "alacritty",
+                "konsole",
+                "gnome-terminal",
+                "xfce4-terminal",
+            ]
+            .into_iter()
+            .map(PathBuf::from),
+        );
+
+    candidates.find_map(|candidate| {
+        let executable = find_executable(&candidate)?;
+        let name = candidate.file_name()?.to_str()?;
+        let prefix = terminal_prefix(name)
+            .iter()
+            .map(|arg| (*arg).to_owned())
+            .collect();
+        Some(TerminalCommand { executable, prefix })
+    })
+}
+
+fn find_executable(candidate: &Path) -> Option<PathBuf> {
+    if candidate.is_absolute() || candidate.components().count() > 1 {
+        return candidate.is_file().then(|| candidate.to_owned());
+    }
+
+    let path = env::var_os("PATH")?;
+    env::split_paths(&path)
+        .map(|directory| directory.join(candidate))
+        .find(|path| path.is_file())
+}
+
+// please standardise this
+fn terminal_prefix(name: &str) -> &'static [&'static str] {
+    match name {
+        "kitty" | "gnome-terminal" => &["--"],
+        _ => &["-e"],
     }
 }
